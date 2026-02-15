36ade2d46562121e09d590b373b55cc8804f5a08
Anubhav <singhkrishna6052@gmail.com>
2026-02-15 21:26:07 +0530

    Created a function AuthProvider for user which stored in localStorage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,21 @@
-import { createContext, useContext} from "react";
+import { createContext, useContext,useEffect,useState} from "react";
 
 const AuthContext = createContext();
 
 // Custom hook to use auth anywhere
 export function useAuth() {
   return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  // Check if user already saved in localStorage
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const savedAuth = localStorage.getItem("auth");
+    if (savedAuth === "true") {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
 }
