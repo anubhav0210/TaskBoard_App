0dd2df3b6313d669a2051aa3790aa42718e90062
Anubhav <singhkrishna6052@gmail.com>
2026-02-16 10:53:48 +0530

    Updated App.jsx file with procted routes using login function and get task list of users

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+// This file handles routing of our application
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Board from "./pages/Board";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// Protected Route Component
+// It prevents access to board without login
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  // If user not authenticated redirect to login
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
 }
 
-export default App
+function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/board"
+          element={
+            <ProtectedRoute>
+              <Board />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
+  );
+}
+
+export default App;
