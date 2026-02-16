6474c454d5e8c05a5f6ede3325c209bd1b378227
Anubhav <singhkrishna6052@gmail.com>
2026-02-16 11:20:24 +0530

    Updated the Board.jsx with the inclusion of form by which task is get created with their activity performed by them

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,32 +1,35 @@
 // Board.jsx
 // Main task board layout
 
+import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { useTasks } from "../context/TaskContext";
+import Column from "../components/Column";
+import TaskForm from "../components/TaskForm";
+import ActivityLog from "../components/ActivityLog";
 
 function Board() {
   const { logout } = useAuth();
+  const { resetBoard } = useTasks();
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <div>
       <h2>Task Board</h2>
 
-      {/* Logout button */}
       <button onClick={logout}>Logout</button>
+      <button onClick={() => setShowForm(true)}>Add Task</button>
+      <button onClick={resetBoard}>Reset Board</button>
 
-      {/* Columns will come here next */}
+      {showForm && <TaskForm closeForm={() => setShowForm(false)} />}
+
       <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
-        <div>
-          <h3>Todo</h3>
-        </div>
-
-        <div>
-          <h3>Doing</h3>
-        </div>
-
-        <div>
-          <h3>Done</h3>
-        </div>
+        <Column status="Todo" />
+        <Column status="Doing" />
+        <Column status="Done" />
       </div>
+
+      <ActivityLog />
     </div>
   );
 }
